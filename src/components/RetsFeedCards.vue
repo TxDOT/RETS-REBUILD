547d172e551cf4a6b4d87e6155e87e46736c9e43
dprosack <david.prosack@txdot.gov>
2025-09-24 16:17:20 -0500

    minor detials changes

--- src/components/RetsFeedCards.vue
+++ src/components/RetsFeedCards.vue
@@ -62,7 +62,7 @@
             </v-text-field>
         </div>
         <div id="retsURL" v-if="isShowRetsUrl && !store.isDetailsPage" @click="isShowRetsUrl = false; restoreFilters();">
-            <v-banner icon="mdi-restore" text="Filter settings updated. Click to restore." id="retsURLBanner" @click="">
+            <v-banner icon="mdi-restore" text="Filter settings updated. Click to restore." id="retsURLBanner" >
                 <!-- <template v-slot:prepend>
                     <icon icon="mdi-restore"></icon>
                 </template> -->
